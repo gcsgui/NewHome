dfac558b5e6081eea2cea675de724eaf9f17d87a
gcsgui <guilherme.cardoso.data@outlook.com>
2026-07-12 16:36:52 -0300

    Reserva 1

--- reservas.js
+++ reservas.js
@@ -1,5 +1,5 @@
 // reservas.js - adicione aqui os códigos dos presentes reservados
-window.PRESENTES_RESERVADOS = [
+window.PRESENTES_RESERVADOS = ["EL-1007"
   // Exemplo:
   // "FE-4029",
   // "LE-8834"
